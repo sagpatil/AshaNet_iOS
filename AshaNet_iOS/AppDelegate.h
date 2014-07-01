0170289b143e3c32332836c9a816dcd9de02e20b
Sagar Patil <sagpatil@paypal.com>
2014-07-01 08:07:26 -0700

    Initial project created. Pods setup

---
+++ AshaNet_iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AshaNet_iOS
+//
+//  Created by Patil, Sagar on 7/1/14.
+//  Copyright (c) 2014 Patil, Sagar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
