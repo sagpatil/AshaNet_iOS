3a98f3a40322a1bdc8cc23f30c2d3757e5bd1f59
Sagar Patil <sagpatil@paypal.com>
2014-07-05 19:32:31 -0700

    Adding Model for event

---
+++ AshaNet_iOS/Event.m
@@ -0,0 +1,13 @@
+//
+//  Event.m
+//  AshaNet_iOS
+//
+//  Created by Patil, Sagar on 7/5/14.
+//  Copyright (c) 2014 Patil, Sagar. All rights reserved.
+//
+
+#import "Event.h"
+
+@implementation Event
+
+@end
