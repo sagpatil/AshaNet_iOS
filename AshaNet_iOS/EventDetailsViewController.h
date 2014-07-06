4107046362b6f22506c95f19ceafab1577ec14ee
Sumit Savla <susavla@paypal.com>
2014-07-06 13:43:25 -0700

    event detail screen

---
+++ AshaNet_iOS/EventDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailsViewController.h
+//  AshaNet_iOS
+//
+//  Created by Savla, Sumit on 7/6/14.
+//  Copyright (c) 2014 Patil, Sagar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailsViewController : UIViewController
+
+@end
